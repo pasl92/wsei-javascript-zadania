57dac528f1fcf956d884ef3182c3fe18ee9cd2db
Paweł Sałata <pawel.salata@microsoft.wsei.edu.pl>
2020-10-04 18:44:28 +0200

    1 zadania

---
+++ 1.Początki/script.js
@@ -0,0 +1,12 @@
+//zadanie 1
+console.log("Paweł");
+
+//zadanie 2
+console.log(12 + 32); //44
+console.log(52 / 16); //3.25
+console.log(5 * 2); //10
+
+//zadanie 3
+  console.log("Hello World");
+  // Hello World;// blad oczekuje zmiennej
+  //"Hello World; // blad nieskonczony string
